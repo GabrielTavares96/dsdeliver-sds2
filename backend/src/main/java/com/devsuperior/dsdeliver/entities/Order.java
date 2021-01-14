740404dbe148db31fd921b1fd38650592e6e68ed
GabrielTavares96 <gabriel_sbo1996@hotmail.com>
2021-01-14 13:36:57 -0300

    Fix total

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -89,6 +89,14 @@ public class Order implements Serializable {
         return products;
     }
 
+    public Double getTotal() {
+        double sum = 0;
+        for (Product p: products) {
+            sum += p.getPrice();
+        }
+        return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
